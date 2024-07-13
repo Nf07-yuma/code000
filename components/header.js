d3e92adc42b08cdecff89b9189c427de784f6daa
Nf07-yuma <120365854+Nf07-yuma@users.noreply.github.com>
2024-07-13 01:34:47 +0000

    new

--- components/header.js
+++ components/header.js
@@ -8,7 +8,6 @@ export default function Header() {
           <Link href="/">Code:000</Link>
         </div>
         <div className="space-x-4">
-          <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
         </div>
